c61914afc23f33369163eacab8d0094bd4f5e93a
kabil <46300483+kabil16@users.noreply.github.com>
2021-06-05 21:15:52 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,70 @@
-let apiURL ="https://restcountries.eu/rest/v2/all";
 
+ //model 
+ let modelData = document.createElement('div');
+ modelData.className='modal fade';
+ modelData.id='staticBackdrop';
+ modelData.setAttribute('data-bs-backdrop','static')
+ modelData.setAttribute('data-bs-keyboard','false')
+ modelData.setAttribute('tabindex','-1')
+ modelData.setAttribute('aria-labelledby','staticBackdropLabel')
+ modelData.setAttribute('aria-hidden','true')
+ document.querySelector('body').appendChild(modelData);
+
+
+ let mainModel = document.createElement('div');
+ mainModel.className='modal-dialog modal-dialog-centered';
+ modelData.appendChild(mainModel);
+
+ let contentModel = document.createElement('div');
+ contentModel.className='modal-content';
+ mainModel.appendChild(contentModel);
+
+ let modelHeader =document.createElement('div');
+ modelHeader.className='modal-header'
+ contentModel.appendChild(modelHeader);
+
+ let modelHeading = document.createElement('h5');
+ modelHeading.className='modal-title';
+ modelHeading.id='staticBackdropLabel';
+ //let modelTitle = document.createTextNode(wetherCountry);
+ //modelHeading.appendChild(modelTitle);
+ modelHeader.appendChild(modelHeading);
+
+ let modelClose=document.createElement('button');
+ modelClose.className='btn-close';
+ modelClose.setAttribute('type','button');
+ modelClose.setAttribute('data-bs-dismiss','modal');
+ modelClose.setAttribute('aria-label','Close');
+ modelHeader.appendChild(modelClose);
+
+ let modelBody = document.createElement('div');
+ modelBody.className='modal-body';
+ contentModel.appendChild(modelBody);
+ let bodyText =document.createTextNode('model body')
+ modelBody.appendChild(bodyText);
+ contentModel.appendChild(modelBody);
+
+ let body1 =document.createElement('p');
+ body1.className='body1';
+ contentModel.appendChild(body1);
+
+ let body2 =document.createElement('img');
+ body2.className='wicon myImg';
+ contentModel.appendChild(body2);
+
+ let modelFooter = document.createElement('div');
+ modelFooter.className='modal-footer';
+ contentModel.appendChild(modelFooter);
+
+ let mfooterClose =document.createElement('button');
+ mfooterClose.className='btn btn-secondary';
+ mfooterClose.setAttribute('data-bs-dismiss','modal')
+ let mfcText = document.createTextNode('Close');
+ mfooterClose.appendChild(mfcText);
+ modelFooter.appendChild(mfooterClose);
+
+
+ let apiURL ="https://restcountries.eu/rest/v2/all"; 
 fetch(apiURL).then((data)=>{
     return data.json();
 }).then((data)=>{
@@ -13,24 +78,7 @@ fetch(apiURL).then((data)=>{
         let latLong = element.latlng;
         //console.log(latLong);
         //weather
-        let lat = element.latlng[0];
-        let long = element.latlng[1];
-        let wetherKey ="86f45ffb22232a6c7069f06bf87078c5"
-
-        
-       // api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-
-        let wetherURL="https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+long+"&appid="+wetherKey;
-
-        fetch(wetherURL).then((data)=>{
-            return data.json();
-        }).then((data)=>{
-            let counTemp = data;
-            console.log(counTemp)
-            //console.log(cname)
-        }).catch((err)=>{
-            console.log(err)
-        })
+       
         
         //console.log(wetherURL);
         // console.log(cname);
@@ -93,78 +141,40 @@ fetch(apiURL).then((data)=>{
         button.setAttribute('data-bs-toggle','modal');
         button.setAttribute('data-bs-target','#staticBackdrop')
         //event Listner
-        //button.addEventListener("click",weatherFunction);
+        button.addEventListener('click',myFunnction);
         let buttonNote = document.createTextNode("Click for wether");
         button.appendChild(buttonNote);
-        card.appendChild(button);
-
-
-        //model 
-        let modelData = document.createElement('div');
-        modelData.className='modal fade';
-        modelData.id='staticBackdrop';
-        modelData.setAttribute('data-bs-backdrop','static')
-        modelData.setAttribute('data-bs-keyboard','false')
-        modelData.setAttribute('tabindex','-1')
-        modelData.setAttribute('aria-labelledby','staticBackdropLabel')
-        modelData.setAttribute('aria-hidden','true')
-        document.querySelector('body').appendChild(modelData);
-
-
-        let mainModel = document.createElement('div');
-        mainModel.className='modal-dialog modal-dialog-centered';
-        modelData.appendChild(mainModel);
-
-        let contentModel = document.createElement('div');
-        contentModel.className='modal-content';
-        mainModel.appendChild(contentModel);
-
-        let modelHeader =document.createElement('div');
-        modelHeader.className='modal-header'
-        contentModel.appendChild(modelHeader);
-
-        let modelHeading = document.createElement('h5');
-        modelHeading.className='modal-title';
-        modelHeading.id='staticBackdropLabel';
-        let modelTitle = document.createTextNode(cname);
-        modelHeading.appendChild(modelTitle);
-        modelHeader.appendChild(modelHeading);
-
-        let modelClose=document.createElement('button');
-        modelClose.className='btn-close';
-        modelClose.setAttribute('type','button');
-        modelClose.setAttribute('data-bs-dismiss','modal');
-        modelClose.setAttribute('aria-label','Close');
-        modelHeader.appendChild(modelClose);
-
-        let modelBody = document.createElement('div');
-        modelBody.className='modal-body';
-        contentModel.appendChild(modelBody);
-        let bodyText =document.createTextNode('model body')
-        modelBody.appendChild(bodyText);
-        contentModel.appendChild(modelBody);
-
-        let modelFooter = document.createElement('div');
-        modelFooter.className='modal-footer';
-        contentModel.appendChild(modelFooter);
-
-        let mfooterClose =document.createElement('button');
-        mfooterClose.className='btn btn-secondary';
-        mfooterClose.setAttribute('data-bs-dismiss','modal')
-        let mfcText = document.createTextNode('Close');
-        mfooterClose.appendChild(mfcText);
-        modelFooter.appendChild(mfooterClose);
-
-       
-
-         function weatherFunction(){
-                console.log(Hii)
-
-         }
+        card.appendChild(button);    
         
+        function myFunnction(){
+            let lat = element.latlng[0];
+            let long = element.latlng[1];
+            let wetherCountry = element.name;
+            let wetherKey ="86f45ffb22232a6c7069f06bf87078c5"
+           // console.log(wetherCountry);
+           // api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}    
+            let wetherURL="https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+long+"&appid="+wetherKey;
 
-       
-       
+            fetch(wetherURL).then((data)=>{
+                return data.json();
+            }).then((data)=>{
+               // console.log(data)
+                let counTemp = data.main.temp;
+                let weatherIcon=data.weather[0].icon;
+                console.log(weatherIcon)
+                let iconURL="https://openweathermap.org/img/wn/"+weatherIcon+"@2x.png";
+
+                document.querySelector('.modal-body').innerHTML=wetherCountry;
+                document.querySelector('.body1').innerHTML=counTemp;
+                document.querySelector('.wicon').setAttribute('src',iconURL)
+
+                //document.querySelector('.modal-title').innerHTML(wetherCountry);
+                //console.log(cname)
+            }).catch((err)=>{
+                console.log(err)
+            })
+        }
+
     });
 
 }).catch((err)=>{
